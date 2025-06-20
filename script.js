292b63a69040585133bf0f060b43a80611b35151
MarcusDeslandes <marcus_deslandes@hotmail.com>
2025-06-20 12:50:57 -0300

    feat: iniciado código de calculo price - não funcionando corretamente

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 function calcularMensalidade () {
     const valorCasa = document.querySelector("#valor-casa");
 
-    let anosParcela = document.querySelector("#anos-parcela");
-    const mesesParcela = anosParcela * 12;
+    const anosParcela = document.querySelector("#anos-parcela");
+    const mesesParcela = anosParcela.value * 12;
 
     const taxaJuros = document.querySelector('#taxa-juros');
+    const jurosConvertido = taxaJuros.value / 100;
 
-    let valorParcela = (valorCasa * taxaJuros) / (1 - (1 + taxaJuros)** mesesParcela - 12 );
+    const valorParcela = valorCasa.value * (1 + jurosConvertido) ** mesesParcela * jurosConvertido / (1 + jurosConvertido) ** mesesParcela - 1;
+
+    console.log(mesesParcela);
+    console.log(jurosConvertido);
+    
+    console.log(valorParcela);
+    
 }
